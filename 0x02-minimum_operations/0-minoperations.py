8d3711dad7e34a84e02d2fb0ebbd45ced196a117
Lawal Taofik <layomide81@gmail.com>
2023-05-04 21:21:34 +0100

    minimum operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+import math
+
+def minOperations(n):
+    if n == 1:
+        return 0
+    if is_prime(n):
+        return 0
+    dp = [math.inf] * (n+1)
+    dp[1] = 0
+    for i in range(2, n+1):
+        for d in range(2, i):
+            if i % d == 0:
+                dp[i] = min(dp[i], dp[d] + (i//d))
+    return dp[n]
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(math.sqrt(n))+1):
+        if n % i == 0:
+            return False
+    return True
+
